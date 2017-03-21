70447273fec16a5dd74fa20e9c5072278418422c
RobbieBakker <robinb@live.nl>
2017-03-21 21:44:41 +0100

    updated about info

--- resources/views/over.blade.php
+++ resources/views/over.blade.php
@@ -22,7 +22,7 @@
 
         <!-- Intro Content -->
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-6" style="margin-top:70px">
                 <img src="{{ asset('images/Koor1.JPG') }}" />
             </div>
             <div class="col-md-6">
@@ -31,26 +31,27 @@
                 <p>Het idee van oprichting ontstond toen er een zangavond werd georganiseerd in één van de kerken in Hardinxveld, waarvan de opbrengst naar een goed doel zou gaan. Arjan Duijzer was in die gemeente als organist actief en werd gevraagd om te dirigeren. Na de zangavond besloten Arjan en wat zangers om een koor op te richten.</p>
                 <p>Tot oktober 2015 heeft Arjan de leiding gehad over Glory en werden er ongeveer drie concerten per jaar georganiseerd.</p>
                 <p>In november 2015 werd het stokje overgenomen door Mark Brandwijk. Het kerstconcert van december 2016 was de eerste uitvoering onder Marks leiding.</p>
+                <p>Een zangseizoen loopt van eind augustus tot begin juli. Het kan gebeuren dat er in de schoolvakanties ook repetities zijn gepland. Van de leden wordt gevraagd zich voortijdig af te melden. Leden van de vereniging betalen contributie. De contributie bedraagt &euro;100,- per jaar. Dit bedrag kan in één keer voldaan worden, in 2 termijnen van à &euro;51,- of 4 termijnen van à &euro;26,-.</p>
             </div>
         </div>
         <!-- /.row -->
 
         <!-- Intro Content -->
-        <!--<div class="row">
+        <!-- <div class="row">
             <div class="col-lg-12">
                 <h2 class="page-header"></h2>
             </div>
             <div class="col-md-6">
-                <h2>Over CJK Glory</h2>
-                <p>Christelijk jongerenkoor Glory uit Hardinxveld is opgericht in november 2012. Met dit interkerkelijke koor willen de leden zingen tot eer en glorie van God. Het koor bestaat uit ongeveer vijfentwintig leden in de leeftijd van vijftien tot vijfendertig jaar.</p>
-                <p>Het idee van oprichting ontstond toen er een zangavond werd georganiseerd in één van de kerken in Hardinxveld, waarvan de opbrengst naar een goed doel zou gaan. Arjan Duijzer was in die gemeente als organist actief en werd gevraagd om te dirigeren. Na de zangavond besloten Arjan en wat zangers om een koor op te richten.</p>
-                <p>Tot oktober 2015 heeft Arjan de leiding gehad over Glory en werden er ongeveer drie concerten per jaar georganiseerd.</p>
-                <p>In november 2015 werd het stokje overgenomen door Mark Brandwijk. Het kerstconcert van december 2016 was de eerste uitvoering onder Marks leiding.</p>
+                <h2>Praktische info</h2>
+                <p><b>Kosten: </b> &euro;100,- op jaarbasis</p>
+                <p><b>Tijden: </b></p>
+                <p></p>
+                <p></p>
             </div>
             <div class="col-md-6">
                 <img class="img-responsive" src="../images/sheetmusic.jpg" alt="">
             </div>
-        </div>-->
+        </div> -->
         <!-- /.row -->
 
 
